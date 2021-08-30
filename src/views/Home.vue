0948b8acdf1dabb3f94a6ae19b81b2e8fc82db15
caizeping <caizeping@dm-ai.cn>
2021-08-30 11:47:08 +0800

    测试提交信息是否有校验

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <a-button>test</a-button>
+    <a-button>test11</a-button>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
