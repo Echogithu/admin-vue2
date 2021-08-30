232f48efabc79c38d65048f8d2c4ae2bb5829ded
caizeping <caizeping@dm-ai.cn>
2021-08-30 11:29:39 +0800

    封装axios

--- src/plugins/registComponents.js
+++ src/plugins/registComponents.js
@@ -3,18 +3,11 @@
 另外因为是定制主题的方式，还需要在vue.config.js中配置less的相关参数，
 其中javascriptEnabled的值必须为true，否则主题定制的样式无法生效
  */
-import Vue from "vue";
-import {
-  Button,
-  Input,
-} from "ant-design-vue";
-
+import Vue from 'vue';
+import { Button, Input } from 'ant-design-vue';
 
 /*需要注册的组件名字, 注册之前需要确保已经import这个组件*/
-const components = [
-  Button,
-  Input,
-];
+const components = [Button, Input];
 register(components);
 
 function register(components) {
---
+++ src/utils/request.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { notification } from 'ant-design-vue';
+import { VueAxios } from './axios';
+
+// 创建 axios 实例
+const service = axios.create({
+  baseURL: '/api', // api base_url
+  timeout: 15000 // 请求超时时间
+});
+
+const err = error => {
+  console.log('捕获到错误');
+  console.log(error);
+  if (error.message === 'timeout of 15000ms exceeded') {
+    notification.error({
+      message: '网络连接超时',
+      description: error.message,
+      duration: 0
+    });
+  } else {
+    notification.error({
+      message: error.response.status,
+      description: error.response.data,
+      duration: 0
+    });
+  }
+  return Promise.reject(error);
+};
+
+// request interceptor
+service.interceptors.request.use(config => {
+  // const token = Vue.ls.get(TOKEN_NAME)
+  // if (token) {
+  //   config.headers['Access-Token'] = token // 让每个请求携带自定义 token 请根据实际情况自行修改
+  // }
+  return config;
+}, err);
+
+// response interceptor
+service.interceptors.response.use(response => {
+  return response.data;
+}, err);
+
+const installer = {
+  vm: {},
+  install(Vue) {
+    Vue.use(VueAxios, service);
+  }
+};
+
+export { installer as VueAxios, service as axios };
